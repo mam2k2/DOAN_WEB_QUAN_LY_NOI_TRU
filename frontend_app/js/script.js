06cbc58a12dfcdae0d84e1198729fc5b10463784
Hiếu <55701970+vudanghieu1@users.noreply.github.com>
2025-05-20 21:39:03 +0700

    fix html

--- frontend_app/js/script.js
+++ frontend_app/js/script.js
@@ -7,3 +7,33 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+function loadPage(page) {
+document.getElementById('page-frame').src = page;
+}
+
+
+
+// ----------------------User_dropdown-------------------------
+function toggleUserMenu() {
+    const menu = document.getElementById("userDropdown");
+    menu.style.display = (menu.style.display === "block") ? "none" : "block";
+  }
+
+  function goToProfile() {
+    alert("Đi đến trang hồ sơ (chưa gắn link)");
+    // Ví dụ: window.location.href = 'profile.html';
+  }
+
+  function logout() {
+    alert("Đăng xuất...");
+    // Ví dụ: window.location.href = 'logout.php';
+  }
+
+  // Ẩn dropdown nếu click ra ngoài
+  window.addEventListener('click', function(e) {
+    const menu = document.getElementById("userDropdown");
+    if (!e.target.closest('.user-menu')) {
+      menu.style.display = "none";
+    }
+  });
